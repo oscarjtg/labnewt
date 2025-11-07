16a0a48ba141d73ba2388f1b7799831bff019531
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-07 08:49:26 +0000

    Added x-velocity component calculation and test

--- labnewt/_macroscopic.py
+++ labnewt/_macroscopic.py
@@ -20,3 +20,25 @@ def _density(f):
         Two-dimensional numpy array containing density values.
     """
     return np.sum(f, axis=0)
+
+
+def _velocity_x(f, s):
+    """
+    Calculates the x-component of macroscopic velocity from distribution function `f`.
+
+        u[y, x] = sum_q f[q, y, x] * s.cx[q]
+
+    Parameters
+    ----------
+    f : np.ndarray
+        Three-dimensional numpy array containing distribution function values.
+
+    s : Stencil
+        Stencil instance, which contains attribute cx which is a one-dimensional numpy array.
+
+    Returns
+    -------
+    u : np.ndarray
+        Two-dimensional numpy array containins x-component of velocity values.
+    """
+    return np.sum(f * s.cx[:, None, None], axis=0)
--- tests/test_macroscopic.py
+++ tests/test_macroscopic.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from labnewt._macroscopic import _density
+from labnewt import StencilD2Q9
+from labnewt._macroscopic import _density, _velocity_x
 
 
 def test_density_d2q9_against_known_values():
@@ -8,16 +9,37 @@ def test_density_d2q9_against_known_values():
     f = np.empty((nq, 2, 2))
     for q in range(nq):
         f[q, 0, 0] = 0.1
-        f[q, 1, 0] = 0.1 * (q + 1)
-        f[q, 0, 1] = 0.1 if q % 2 == 0 else -0.1
+        f[q, 0, 1] = 0.1 * (q + 1)
+        f[q, 1, 0] = 0.1 if q % 2 == 0 else -0.1
         f[q, 1, 1] = 0.1 * (q + 1) if q % 2 == 0 else -0.1 * (q + 1)
 
     r_exp = np.empty((2, 2))
     r_exp[0, 0] = 0.9  # 9 * 0.1
-    r_exp[1, 0] = 4.5  # 0.5 * 9 * 10 / 10
-    r_exp[0, 1] = 0.1
+    r_exp[0, 1] = 4.5  # 0.5 * 9 * 10 / 10
+    r_exp[1, 0] = 0.1
     r_exp[1, 1] = 0.5
 
     r_com = _density(f)
 
     assert np.allclose(r_exp, r_com, atol=1.0e-12)
+
+
+def test_velocity_x_d2q9_against_known_values():
+    s = StencilD2Q9
+    nq = 9
+    f = np.empty((nq, 2, 2))
+    for q in range(nq):
+        f[q, 0, 0] = 0.0
+        f[q, 0, 1] = 0.1
+        f[q, 1, 0] = 0.1 if s.ex[q] > 0 else 0.0
+        f[q, 1, 1] = 0.1 if s.ex[q] > 0 else -0.1
+
+    u_exp = np.empty((2, 2))
+    u_exp[0, 0] = 0.0  # all f = 0
+    u_exp[0, 1] = 0.0  # f same in all directions, so no net speed
+    u_exp[1, 0] = 0.3  # only three vectors in positive x direction
+    u_exp[1, 1] = 0.6  # three +ve vals in +ve x, three -ve vals in -ve x
+
+    u_com = _velocity_x(f, s)
+
+    assert np.allclose(u_exp, u_com, atol=1.0e-12)
