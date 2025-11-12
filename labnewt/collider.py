434a7487ea379039b15670caed86899ae048c4af
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-12 09:20:13 +0000

    ColliderSRT.collide() now acts on separate fi, fo

--- labnewt/collider.py
+++ labnewt/collider.py
@@ -6,24 +6,31 @@ class ColliderSRT:
         tau_star = 0.5 + 3.0 * dt * nu / (dx**2)
         self.omega = 1.0 / tau_star
 
-    def collide(self, f, r, u, v, s):
+    def collide(self, fi, fo, r, u, v, s):
         """
         Apples the single relaxation time collision algorithm.
 
+        Modifies fo in place. Keeps fi constant.
+
         Parameters:
         ----------
-        f : np.ndarray
-            Three-dimensional numpy array of floats
-            containing particle distribution values.
+        fi : np.ndarray
+            Three-dimensional numpy array containing f_in[q, y, x]
+
+        fo : np.ndarray
+            Three-dimensional numpy array containing f_out[q, y, x]
+
+        r : np.ndarray
+            Two dimensional numpy array containing rho[y, x]
+
+        u : np.ndarray
+            Two dimenaional numpy array containing u[y, x]
+
+        v : np.ndarray
+            Two dimensional numpy array containing v[y, x]
 
         s : Stencil
             A stencil instance.
 
-        Returns:
-        --------
-        np.ndarray:
-            Three-dimensional numpy array of floats
-            containing the particle distribution values
-            after collision.
         """
-        return self.omega * _feq2(r, u, v, s) + (1 - self.omega) * f
+        fo[:] = self.omega * _feq2(r, u, v, s) + (1 - self.omega) * fi
--- tests/test_collider.py
+++ tests/test_collider.py
@@ -27,12 +27,13 @@ def test_collider_srt_unit_omega_stationary_fluid():
     ny = 10
     shape = (nx, ny)
 
-    f = np.random.rand(s.nq, *shape)
+    fi = np.random.rand(s.nq, *shape)
+    fo = np.empty_like(fi)
     r = np.ones(shape)
     u = np.zeros(shape)
     v = np.zeros(shape)
-    f_coll = collider.collide(f, r, u, v, s)
-    assert np.allclose(f_coll, s.w[:, None, None], atol=1.0e-12)
+    collider.collide(fi, fo, r, u, v, s)
+    assert np.allclose(fo, s.w[:, None, None], atol=1.0e-12)
 
 
 def test_collider_srt_conserves_moments():
@@ -43,15 +44,16 @@ def test_collider_srt_conserves_moments():
     ny = 10
     shape = (nx, ny)
 
-    f = np.random.rand(s.nq, *shape)
-    r_pre = _density(f)
-    u_pre = _velocity_x(f, r_pre, s)
-    v_pre = _velocity_y(f, r_pre, s)
+    fi = np.random.rand(s.nq, *shape)
+    fo = np.empty_like(fi)
+    r_pre = _density(fi)
+    u_pre = _velocity_x(fi, r_pre, s)
+    v_pre = _velocity_y(fi, r_pre, s)
 
-    f_coll = collider.collide(f, r_pre, u_pre, v_pre, s)
-    r_post = _density(f_coll)
-    u_post = _velocity_x(f_coll, r_post, s)
-    v_post = _velocity_y(f_coll, r_post, s)
+    collider.collide(fi, fo, r_pre, u_pre, v_pre, s)
+    r_post = _density(fo)
+    u_post = _velocity_x(fo, r_post, s)
+    v_post = _velocity_y(fo, r_post, s)
 
     assert np.allclose(r_pre, r_post, atol=1.0e-12)
     assert np.allclose(u_pre, u_post, atol=1.0e-12)
