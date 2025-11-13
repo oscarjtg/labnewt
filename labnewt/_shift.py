a5d931c75c4b91c42a4634f53ac8bb05683c4511
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-13 16:48:08 +0000

    Documentation

--- labnewt/_shift.py
+++ labnewt/_shift.py
@@ -3,20 +3,22 @@ import numpy as np
 
 def periodic_shift(arr, s, q):
     """
+    Rolls array `arr` in direction (s.ey[q], s.ex[q]).
+
+    Returns a new array. Does not modify `arr`.
+
     Parameters
     ----------
     arr : np.ndarray
-        Two dimensional numpy array
-
+        Two-dimensional numpy array.
     s : Stencil
-        Stencil
-
+        Lattice stencil.
     q : int
-        Lattice vector
+        Lattice direction index.
 
     Returns
     -------
     arr_shifted : np.ndarray
-        arr_shifted[y, x] = arr[(y - s.ey[q]) % Ny, (x - s.ex[q]) % Nx]
+        arr_shifted[y, x] = arr[(y - s.ey[q]) % Ny, (x - s.ex[q]) % Nx].
     """
     return np.roll(np.roll(arr, s.ey[q], axis=0), s.ex[q], axis=1)
