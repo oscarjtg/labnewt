9b7f8762b68c06480fbe6041ed66f6ed86cfd7a1
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-23 16:57:39 +0000

    Added basic callback test

---
+++ tests/unit/test_callback.py
@@ -0,0 +1,18 @@
+from labnewt.callback import Callback
+
+
+class DummyModel:
+    pass
+
+
+def myfunc(model):
+    return 2
+
+
+def test_callback_call():
+    cb = Callback(myfunc, 10, True)
+    model = DummyModel
+    
+    assert cb.on_init == True
+    assert cb.interval == 10
+    assert cb(model) == 2
