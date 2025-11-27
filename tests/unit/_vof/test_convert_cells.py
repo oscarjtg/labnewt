4c4a030e2fadfadabb39fb6497320b730747f441
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-27 17:26:41 +0000

    Added unit tests for over- and underfill identification functions

--- tests/unit/_vof/test_convert_cells.py
+++ tests/unit/_vof/test_convert_cells.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-from labnewt._vof._convert_cells import _convert_cells_, _convert_FI_, _convert_GI_
+from labnewt._vof._convert_cells import (
+    _convert_cells_,
+    _convert_FI_,
+    _convert_GI_,
+    _identify_overfull_,
+    _identify_underfull_,
+)
 
 
 def test_convert_GI_linear_interface():
@@ -209,3 +215,86 @@ def test_convert_cells_corner_to_gas():
     # Check no unintended side effects
     assert np.array_equal(cIF, cIF_copy)
     assert np.array_equal(cIG, cIG_copy)
+
+
+def test_identify_underfull_random_():
+    shape = (3, 4)
+    rng = np.random.default_rng(42)
+    to_gas = rng.random(shape) > 0.5
+    I_mask = rng.random(shape) > 0.5
+    M = rng.normal(0, 1, size=shape)
+    eps = 1.0e-04
+
+    to_gas_id = id(to_gas)
+    I_mask_id = id(I_mask)
+    M_id = id(M)
+
+    I_mask_init = np.copy(I_mask)
+    M_init = np.copy(M)
+
+    _identify_underfull_(to_gas, I_mask, M, eps)
+
+    def slow_version(I_mask, M, eps):
+        ny, nx = I_mask.shape
+        to_gas = np.zeros_like(I_mask, dtype=bool)
+        for j in range(ny):
+            for i in range(nx):
+                if I_mask[j, i] and M[j, i] < eps:
+                    to_gas[j, i] = True
+                else:
+                    to_gas[j, i] = False
+        return to_gas
+
+    expected = slow_version(I_mask, M, eps)
+
+    # Check correct values
+    assert np.allclose(to_gas, expected)
+
+    # Check no unintended side effects
+    assert np.allclose(I_mask, I_mask_init)
+    assert np.allclose(M, M_init)
+    assert id(to_gas) == to_gas_id
+    assert id(I_mask) == I_mask_id
+    assert id(M) == M_id
+
+
+def test_identify_overfull_random_():
+    shape = (3, 4)
+    rng = np.random.default_rng(42)
+    to_fluid = rng.random(shape) > 0.5
+    I_mask = rng.random(shape) > 0.5
+    M = rng.normal(0, 1, size=shape)
+    rho = rng.random(shape)
+    eps = 1.0e-04
+
+    to_fluid_id = id(to_fluid)
+    I_mask_id = id(I_mask)
+    M_id = id(M)
+
+    I_mask_init = np.copy(I_mask)
+    M_init = np.copy(M)
+
+    _identify_overfull_(to_fluid, I_mask, M, rho, eps)
+
+    def slow_version(I_mask, M, rho, eps):
+        ny, nx = I_mask.shape
+        to_fluid = np.zeros_like(I_mask, dtype=bool)
+        for j in range(ny):
+            for i in range(nx):
+                if I_mask[j, i] and M[j, i] > rho[j, i] + eps:
+                    to_fluid[j, i] = True
+                else:
+                    to_fluid[j, i] = False
+        return to_fluid
+
+    expected = slow_version(I_mask, M, rho, eps)
+
+    # Check correct values
+    assert np.allclose(to_fluid, expected)
+
+    # Check no unintended side effects
+    assert np.allclose(I_mask, I_mask_init)
+    assert np.allclose(M, M_init)
+    assert id(to_fluid) == to_fluid_id
+    assert id(I_mask) == I_mask_id
+    assert id(M) == M_id
