8c7622ebedd09ddbe03f60e4ebdd4841dabde20e
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-28 21:49:07 +0000

    Make equilibrium helper functions take single floats as well as numpy arrays

--- labnewt/_equilibrium.py
+++ labnewt/_equilibrium.py
@@ -1,14 +1,17 @@
+"""Define helper functions for calculating equilibrium distribution values."""
+
+
 def _feq2(r, u, v, s):
     """
     Compute the 2nd-order equilibrium distribution function.
 
     Parameters
     ----------
-    r : np.ndarray
+    r : np.ndarray or float
         Two-dimensional array containing the fluid density.
-    u : np.ndarray
+    u : np.ndarray or float
         Two-dimensional array containing the x-component of the fluid velocity.
-    v : np.ndarray
+    v : np.ndarray or float
         Two-dimensional array containing the y-component of the fluid velocity.
     s : Stencil
         The lattice stencil.
@@ -18,13 +21,9 @@ def _feq2(r, u, v, s):
     np.ndarray
         Three-dimensional array containing equilibrium distribution function values.
     """
-    cu = s.cx[:, None, None] * u[None, :, :] + s.cy[:, None, None] * v[None, :, :]
+    cu = s.cx[:, None, None] * u + s.cy[:, None, None] * v
     uu = u**2 + v**2
-    return (
-        s.w[:, None, None]
-        * r[None, :, :]
-        * (1 + 3 * cu + 4.5 * cu**2 - 1.5 * uu[None, :, :])
-    )
+    return s.w[:, None, None] * r * (1 + 3 * cu + 4.5 * cu**2 - 1.5 * uu)
 
 
 def _feq2_q(q, r, u, v, s):
@@ -35,11 +34,11 @@ def _feq2_q(q, r, u, v, s):
     ----------
     q : int
         Lattice vector index.
-    r : np.ndarray
+    r : np.ndarray or float
         Two-dimensional array containing the fluid density.
-    u : np.ndarray
+    u : np.ndarray or float
         Two-dimensional array containing the x-component of the fluid velocity.
-    v : np.ndarray
+    v : np.ndarray or float
         Two-dimensional array containing the y-component of the fluid velocity.
     s : Stencil
         The lattice stencil.
@@ -49,6 +48,6 @@ def _feq2_q(q, r, u, v, s):
     np.ndarray
         Three-dimensional array containing equilibrium distribution function values.
     """
-    cu = s.cx[q] * u + s.cy[q] * v[:, :]
+    cu = s.cx[q] * u + s.cy[q] * v
     uu = u**2 + v**2
     return s.w[q] * r * (1 + 3 * cu + 4.5 * cu**2 - 1.5 * uu)
