bb916f48d85e5f79217834b11f585010a6a1d2d7
oscarjtg <78657968+oscarjtg@users.noreply.github.com>
2025-11-29 09:36:24 +0000

    Add LocalAverageRefiller class

--- labnewt/refiller/__init__.py
+++ labnewt/refiller/__init__.py
@@ -1,5 +1,6 @@
 """Refiller module: exports Refiller class and subclasses"""
 
+from .local_average import LocalAverageRefiller
 from .uniform import UniformRefiller
 
-__all__ = ["UniformRefiller"]
+__all__ = ["LocalAverageRefiller", "UniformRefiller"]
